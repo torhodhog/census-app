d12a56927ed6d1c9d1f74e1f42e6cc7a3c8a6fdb
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-05-12 16:23:42 +0200

    updated server.js

--- server.js
+++ server.js
@@ -83,10 +83,7 @@ app.post('/participants/add', auth, (req, res) => {
 
  
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
 
 
 let participants = [
